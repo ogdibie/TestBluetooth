95eaf28732ff485d3f4a55229ccbf5fbac91408c
Ogheneruona Dibie <ogheneruona.dibie@gmail.com>
2015-06-08 19:34:23 -0400

    Added the ConnectedThread in order to enable for a seperate thread to be used in communication. Note, both the read and write methods are blocking call. Also added a method to display error on the screen for several try catch methods

--- app/src/main/java/testbluetooth/com/example/ogheneruona/testbluetooth/MainActivity.java
+++ app/src/main/java/testbluetooth/com/example/ogheneruona/testbluetooth/MainActivity.java
@@ -1,12 +1,50 @@
 package testbluetooth.com.example.ogheneruona.testbluetooth;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 
 public class MainActivity extends ActionBarActivity {
+    //given name of the bluetooth used in tagging
+    private static final String TAG = "bluetooth1";
+
+    final int RECIEVE_MESSAGE = 1;        // Status  for Handler
+    //Buttons to control LED
+    private Button btnOn, btnOff;
+
+    //text editor to display recieved message
+    private TextView txtArduino;
+    Handler h;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+
+    //String builder used in building string like Stringbuffer but has overloaded methods
+    // for insertion ann appending as well
+    private StringBuilder sb = new StringBuilder();
+
+    private ConnectedThread mConnectedThread;
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    // SPP UUID service used in creating RFComm Channel
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    // MAC-address of Bluetooth module (you must edit this line for the particular bluetooth)
+    private static String address = "10:14:05:22:04:97";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +72,59 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Used to create a toast message to be displayed on the screen with an error when a problem occurs
+    private void errorExit(String title, String message){
+        Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                errorExit("Error in stream", "Could not get the input or outputstream data");
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[256];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);        // Get number of bytes and message in "buffer"
+                    h.obtainMessage(RECIEVE_MESSAGE, bytes, -1, buffer).sendToTarget();     // Send to message queue Handler
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        public void write(String message) {
+            Log.d(TAG, "...Data to send: " + message + "...");
+            byte[] msgBuffer = message.getBytes();
+            try {
+                mmOutStream.write(msgBuffer);
+            } catch (IOException e) {
+                Log.d(TAG, "...Error data send: " + e.getMessage() + "...");
+            }
+        }
     }
 }
